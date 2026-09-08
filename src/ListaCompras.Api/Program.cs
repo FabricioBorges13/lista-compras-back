fd7ce5fb28675344c2ccd8ef8cddde9e68431760
fabricio-borges <fabricio.riff2@gmail.com>
2026-09-08 16:50:20 -0300

    feat: configurar EF Core com SQLite
    - Instalar pacotes Microsoft.EntityFrameworkCore.Sqlite e Microsoft.EntityFrameworkCore.Design
    - Criar AppDbContext em Infrastructure/Data
    - Configurar connection string no appsettings.json
    - Registrar DbContext no DI (Program.cs)
    - Criar migration inicial
    - Aplicar migration com dotnet ef database update
    
    Banco de dados criado e migration aplicada com sucesso.

--- src/ListaCompras.Api/Program.cs
+++ src/ListaCompras.Api/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ListaCompras.Infrastructure.Data.AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
